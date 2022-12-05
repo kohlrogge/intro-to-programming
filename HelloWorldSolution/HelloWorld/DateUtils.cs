67deafa17f6863c04cbaaaae74eed40eb773cace
Karmell Ohlrogge <KarmellOhlrogge@gmail.com>
2022-12-05 15:22:35 -0500

    changed error message

---
+++ HelloWorldSolution/HelloWorld/DateUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class DateUtils
+    {
+        public DateTime TakeABreak(int minutes)
+        {
+            return DateTime.Now.AddMinutes(minutes);
+        }
+    }
+}
---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Console.WriteLine("Take a break!");
+Console.WriteLine("How many minutes? :");
+string minutes = Console.ReadLine();
+
+if (minutes is not null)
+{
+    DateUtils utils= new DateUtils();
+
+    int mins = int.Parse(minutes);
+    DateTime timeAtEndOfBreak = utils.TakeABreak(mins);
+    Console.WriteLine($"Ok, be back at {timeAtEndOfBreak:T}");
+}
+else
+{
+    Console.WriteLine("eneter some minutes, moron");
+}
